4f0e8148581574216db215859e99e354d1e106cb
Hiromi ISHII <konn.jinro@gmail.com>
2018-10-14 07:04:44 +0900

    Simple parsing examples

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
+extern crate serde_json;
 extern crate yas_to_vscode;
+use std::fs;
 use yas_to_vscode::*;
 
 fn main() {
-    println!("Hello, world!");
+    let src = fs::read_to_string("data/type-role").unwrap();
+    println!(
+        "{}",
+        serde_json::to_string(&Snippet::parse(&src).unwrap().result).unwrap()
+    )
 }
